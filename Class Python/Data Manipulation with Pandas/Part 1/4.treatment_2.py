8e99a81bbd8866a3f5a7d7504b957093eb1f7293
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-29 11:42:37 +0700

    Add script

--- Class Python/Data Manipulation with Pandas/Part 1/4.treatment_2.py
+++ Class Python/Data Manipulation with Pandas/Part 1/4.treatment_2.py
@@ -1,1 +1,17 @@
+import pandas as pd
+# Baca file "https://storage.googleapis.com/dqlab-dataset/datacovid19.csv"
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/datacovid19.csv")
+# Cetak ukuran awal dataframe
+print("Ukuran awal df: %d baris, %d kolom." % df.shape)
+# Drop kolom yang seluruhnya missing value dan cetak ukurannya
+df = df.dropna(axis=1, how='all')
+print("Ukuran df setelah buang kolom dengan seluruh data missing: %d baris, %d kolom." % df.shape)
+# Drop baris jika ada satu saja data yang missing dan cetak ukurannya
+df = df.dropna(axis=0, how='any')
+print("Ukuran df setelah dibuang baris yang memiliki sekurangnya 1 missing value: %d baris, %d kolom." % df.shape)
 
+# method .dropna(): axis dan how. Keyword axis digunakan untuk menentukan arah dataframe yang akan dibuang angka 1 untuk menyatakan kolom (column-based)
+#  Jika digunakan angka 0 berarti itu dalam searah index (row-based)
+# Sementara, keyword how digunakan untuk bagaimana cara membuangnya. Opsi yang dapat diterimanya (dalam string) adalah
+#  "all" artinya jika seluruh data di satu/beberapa kolom atau di satu/beberapa baris adalah missing value.
+#  "any" artinya jika memiliki 1 saja data yang hilang maka buanglah baris/kolom tersebut.
