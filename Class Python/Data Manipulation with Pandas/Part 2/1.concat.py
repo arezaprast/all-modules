30bf083df59cbc4b7f3beeb5bad5dbbec456a94c
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-30 11:06:18 +0700

    Add script

--- Class Python/Data Manipulation with Pandas/Part 2/1.concat.py
+++ Class Python/Data Manipulation with Pandas/Part 2/1.concat.py
@@ -1,9 +1,9 @@
 import pandas as pd
 # Buat dataframe df1 dan df2
 df1 = pd.DataFrame({'a':[1,2],
-					'b':[3,4]})
+		    'b':[3,4]})
 df2 = pd.DataFrame({'b':[1,2],
-					'a':[3,4]})
+		    'a':[3,4]})
 # Terapkan method concat row-wise
 row_wise_concat = pd.concat([df1,df2])
 print("Row-wise - concat:\n", row_wise_concat)
