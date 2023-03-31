5c48466ab80097136e9e2617543d01ce801f4bdc
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-31 14:13:30 +0700

    Add script

--- Class Python/Data Manipulation with Pandas/Part 2/mini_project_5.py
+++ Class Python/Data Manipulation with Pandas/Part 2/mini_project_5.py
@@ -1,1 +1,8 @@
-
+# [9]. Plot untuk hasil pada langkah #[8]
+by_order_monthly_mean.plot(
+   figsize = (8,5),
+   title = 'Average Daily order Size in Month View for all Province'
+)
+plt.ylabel('avg order size')
+plt.xlabel('month')
+plt.show()
