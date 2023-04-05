268d7b520ffc45ccabb9f992e2782275516f68c8
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-04-05 18:07:57 +0700

    Add script

--- Class Python/Machine Learning With Python for Beginner/case_study_5.py
+++ Class Python/Machine Learning With Python for Beginner/case_study_5.py
@@ -9,7 +9,7 @@ X = data.drop(['Ad Topic Line','City','Country','Timestamp','Clicked on Ad'], ax
 y = data['Clicked on Ad']
 
 #6b. splitting the data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
 #6c. Modelling
 # Call the classifier
