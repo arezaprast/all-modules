9fbd0fecd9d3fce29fdca66c6172570069eacbc0
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-04-27 13:03:11 +0700

    Add script

--- Projects/Eksplorasi dan Analisis Data COVID-19 Indonesia using Python/3.grafik_sembuh.py
+++ Projects/Eksplorasi dan Analisis Data COVID-19 Indonesia using Python/3.grafik_sembuh.py
@@ -4,16 +4,14 @@ import matplotlib.dates as mdates
 plt.clf()
 fig, ax = plt.subplots(figsize=(10,5))
 ax.bar(data=cov_jabar_tidy, x='tanggal', height='sembuh', color='olivedrab')
-ax.set_title('Kasus Harian Sembuh Dari COVID-19 di Jawa Barat',fontsize=22)
+ax.set_title('Kasus Harian Sembuh Dari COVID-19 di Jawa Barat', fontsize=22)
 ax.set_xlabel('')
 ax.set_ylabel('Jumlah kasus')
-ax.text(1, -0.3, 'Sumber data: covid.19.go.id', color='blue',
-        ha='right', transform=ax.transAxes)
+ax.text(1, -0.3, 'Sumber data: covid.19.go.id', color='blue', ha='right', 
+        transform=ax.transAxes)
 ax.set_xticklabels(ax.get_xticks(), rotation=90)
-
 ax.xaxis.set_major_locator(mdates.MonthLocator())
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
-
 plt.grid(axis='y')
 plt.tight_layout()
 plt.show()
