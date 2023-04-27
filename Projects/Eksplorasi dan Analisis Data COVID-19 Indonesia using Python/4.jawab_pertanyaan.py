3c073e76ecab692b94cd5b173d0bb156f075a3cc
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-04-27 13:20:23 +0700

    Add script

---
+++ Projects/Eksplorasi dan Analisis Data COVID-19 Indonesia using Python/4.jawab_pertanyaan.py
@@ -0,0 +1,4 @@
+cov_jabar_pekanan['jumlah_pekanlalu'] = cov_jabar_pekanan['jumlah'].shift().replace(np.nan, 0).astype(np.int)
+cov_jabar_pekanan['lebih_baik'] = cov_jabar_pekanan['jumlah'] < cov_jabar_pekanan['jumlah_pekanlalu']
+
+print('Sepuluh data teratas:\n', cov_jabar_pekanan.head(10))
