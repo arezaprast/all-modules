b75863352f1e25c77a05f12f4d92fe280b05b91d
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-08-21 13:34:56 +0700

    Add script

--- Class SQL/Menggunakan Subquery pada SQL /Subquery-dalam-Where.sql
+++ Class SQL/Menggunakan Subquery pada SQL /Subquery-dalam-Where.sql
@@ -2,25 +2,16 @@
 SELECT COUNT(DISTINCT customer_id) total_buyer_Sepatu_Jaket
 FROM data_retail
 WHERE product = 'Sepatu' 
-AND customer_id IN (
- 	SELECT distinct Customer_ID
-    FROM data_retail
-    WHERE product = 'Jaket');
+AND customer_id IN (SELECT distinct Customer_ID FROM data_retail WHERE product = 'Jaket');
 
 #Menghitung customer yang membeli Sepatu dan Baju
 SELECT COUNT(DISTINCT customer_id) total_buyer_Sepatu_Baju
 FROM data_retail
 WHERE product = 'Sepatu' 
-AND customer_id IN (
- 	SELECT distinct Customer_ID
-    FROM data_retail
-    WHERE product = 'Baju');
+AND customer_id IN (SELECT distinct Customer_ID FROM data_retail WHERE product = 'Baju');
     
 #Menghitung customer yang membeli Sepatu dan Tas
 SELECT COUNT(DISTINCT customer_id) total_buyer_Sepatu_Tas
 FROM data_retail
 WHERE product = 'Sepatu' 
-AND customer_id IN (
- 	SELECT DISTINCT customer_id
-    FROM data_retail
-    WHERE product = 'Tas');
+AND customer_id IN (SELECT DISTINCT customer_id FROM data_retail WHERE product = 'Tas');
