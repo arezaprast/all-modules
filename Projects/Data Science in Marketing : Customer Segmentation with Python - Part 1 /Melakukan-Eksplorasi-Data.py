1c072838253b8880bf98fb7d44a1b5c4e90aedeb
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-09-12 09:41:22 +0700

    Add script

---
+++ Projects/Data Science in Marketing : Customer Segmentation with Python - Part 1 /Melakukan-Eksplorasi-Data.py
@@ -0,0 +1,52 @@
+# Eksplorasi Data Numerik
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='white')
+import pandas as pd
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t")
+plt.clf()
+# Fungsi untuk membuat plot  
+def observasi_num(features):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 9))
+    for i, kol in enumerate(features):
+	    sns.boxplot(df[kol], ax = axs[i][0])
+	    sns.distplot(df[kol], ax = axs[i][1])
+	    axs[i][0].set_title('mean = %.2f\n median = %.2f\n std = %.2f'%(df[kol].mean(), df[kol].median(), df[kol].std()))
+    plt.setp(axs)
+    plt.tight_layout()
+    plt.show()
+# Memanggil fungsi untuk membuat Plot untuk data numerik   
+kolom_numerik = ['Umur','NilaiBelanjaSetahun']
+observasi_num(kolom_numerik)
+
+# Eksplorasi Data Kategorikal
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='white')
+import pandas as pd
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t")
+plt.clf()
+# Menyiapkan kolom kategorikal  
+kolom_kategorikal = ['Jenis Kelamin','Profesi','Tipe Residen']  
+# Membuat canvas
+fig, axs = plt.subplots(3,1,figsize=(7,10)) 
+# Membuat plot untuk setiap kolom kategorikal  
+for i, kol in enumerate(kolom_kategorikal):  
+    # Membuat Plot
+    sns.countplot(df[kol], order = df[kol].value_counts().index, ax = axs[i])  
+    axs[i].set_title('\nCount Plot %s\n'%(kol), fontsize=15)  
+    # Memberikan anotasi  
+    for p in axs[i].patches:  
+        axs[i].annotate(format(p.get_height(), '.0f'),  
+                        (p.get_x() + p.get_width() / 2., p.get_height()),  
+                        ha = 'center',  
+                        va = 'center',  
+                        xytext = (0, 10),  
+                        textcoords = 'offset points') 
+    # Setting Plot  
+    sns.despine(right=True,top = True, left = True)  
+    axs[i].axes.yaxis.set_visible(False) 
+    plt.setp(axs[i])
+    plt.tight_layout()
+# Tampilkan plot
+plt.show()
