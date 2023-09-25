2a66a87048733bfef1b646096ca815950c94aca6
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-09-25 13:51:57 +0700

    Add script

---
+++ Python/Fundamental/Data Manipulation with Pandas - Part 2/Gabung-Series-Dataframe.py
@@ -0,0 +1,104 @@
+# =====  APPEND  =====
+# .append() dapat digunakan pada dataframe/series yang ditujukan untuk
+#  menambah row-nya saja.
+import pandas as pd
+# Buat series of int (s1) dan series of string (s2)
+s1 = pd.Series([1,2,3,4,5,6])
+s2 = pd.Series(["a","b","c","d","e","f"])
+# Terapkan method append
+s2_append_s1 = s2.append(s1)
+print("Series - append:\n", s2_append_s1)
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({'a':[1,2],
+					'b':[3,4]})
+df2 = pd.DataFrame({'b':[1,2],
+					'a':[3,4]})
+# Terapkan method append
+df2_append_df1 = df2.append(df1)
+print("Dataframe - append:\n", df2_append_df1)
+
+# =====  CONCAT  =====
+# .concat() dapat digunakan pada dataframe yang ditujukan untuk
+#  penggabungan baik dalam row-wise (dalam arah) atau column-wise.
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({'a':[1,2],
+					'b':[3,4]})
+df2 = pd.DataFrame({'b':[1,2],
+					'a':[3,4]})
+# Terapkan method concat row-wise
+row_wise_concat = pd.concat([df2, df1])
+print("Row-wise - concat:\n", row_wise_concat)
+# Terapkan method concat column-wise
+col_wise_concat = pd.concat([df2, df1], axis=1)
+print("Column-wise - concat:\n", col_wise_concat)
+# Penambahan identifier --> membentuk hasil penggabungan multiindex
+multiindex_concat = pd.concat([df2,df1], axis=0, keys=['df2','df1'])
+print("Multiindex - concat:\n", multiindex_concat)
+
+# =====  Merge - Part 1  =====
+# .merge() untuk menggabungkan Series/Dataframe yang bentuknya mirip
+#  dengan syntax join di SQL, specify left and right tables, join key,
+#  dan how to join (left, right, inner, full outer).
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+   'key':['k1','k2','k3','k4','k5'],
+   'val1':[200, 500, 0, 500, 100],
+   'val2':[30, 50, 100, 20, 10]
+})
+df2 = pd.DataFrame({
+   'key':['k1','k3','k5','k7','k10'],
+   'val3':[1,2,3,4,5],
+   'val4':[6,7,8,8,10]
+})
+# Merge yang ekivalen dengan SQL left join
+merge_df_left = pd.merge(left=df1, right=df2, how='left', left_on='key', right_on='key')
+print('Merge - Left:\n', merge_df_left)
+# Merge yang ekivalen dengan SQL right join
+merge_df_right = pd.merge(left=df1, right=df2, how='right', left_on='key', right_on='key')
+print('Merge - Right:\n', merge_df_right)
+# Merge yang ekivalen dengan SQL inner join
+merge_df_inner = pd.merge(left=df1, right=df2, how='inner', left_on='key', right_on='key')
+print('Merge - Inner:\n', merge_df_inner)
+# Merge yang ekivalen dengan SQL outer join
+merge_df_outer = pd.merge(left=df1, right=df2, how='outer', left_on='key', right_on='key')
+print('Merge - Outer:\n', merge_df_outer)
+
+# =====  Merge - Part 2  =====
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+   'key':['k1','k2','k3','k4','k5'],
+   'val1':[200, 500, 0, 500, 100],
+   'val2':[30, 50, 100, 20, 10]
+}).set_index(['key', 'val2'])
+print('Dataframe 1:\n', df1)
+df2 = pd.DataFrame({
+   'key':['k1','k3','k5','k7','k10'],
+   'val3':[1,2,3,4,5],
+   'val4':[6,7,8,8,10]
+}).set_index(['key', 'val3'])
+print('Dataframe 2:\n', df2)
+# Merge dataframe yang memiliki multi index
+df_merge = pd.merge(df1.reset_index(), df2.reset_index())
+print('Merging dataframe:\n', df_merge)
+
+# =====  JOIN  =====
+# .join() digunakan pada dataframe untuk menggabungkan kedua data dengan set index
+#  pada kedua tabel tersebut sebagai join key, tanpa index, hal ini tidak akan berhasil.
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+   'key':['k1','k2','k3','k4','k5'],
+   'val1':[200, 500, 0, 500, 100],
+   'val2':[30, 50, 100, 20, 10]
+})
+df2 = pd.DataFrame({
+   'key':['k1','k3','k5','k7','k10'],
+   'val3':[1,2,3,4,5],
+   'val4':[6,7,8,8,10]
+})
+# Penerapan join dengan menggunakan set_index dan keyword how
+join_df = df1.set_index('key').join(df2.set_index('key'), how='outer')
+print(join_df)
