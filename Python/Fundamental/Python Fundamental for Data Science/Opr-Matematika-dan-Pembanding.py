cfaa89beb6e488570cf6b5a0f28953f3e1831c9b
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-09-26 14:27:54 +0700

    Add script

---
+++ Python/Fundamental/Python Fundamental for Data Science/Opr-Matematika-dan-Pembanding.py
@@ -0,0 +1,24 @@
+# =====  Praktik Operasi Matematika  =====
+a=10
+b=5
+selisih = a-b
+jumlah = a+b
+kali = a*b
+bagi = a/b
+print("Hasil penjumlahan a dan b adalah", jumlah)
+print("Selisih a dan b adalah :",selisih)
+print("Hasil perkalian a dan b adalah :",kali)
+print("Hasil pembagian a dan b adalah:",bagi)
+
+# =====  Operasi modulus  =====
+c=10
+d=3
+modulus=c%d
+print("Hasil modulus",modulus)
+
+# =====  Tugas Mid Praktek  =====
+angka=5
+if(angka%2 == 0):
+    print("angka termasuk bilangan genap")
+else:
+    print("angka termasuk bilangan ganjil")
