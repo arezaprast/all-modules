5584d5926ebd01ab5cab97df2e931535df820e2e
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-09-27 11:06:31 +0700

    Create Mini-Project.py

---
+++ Python/Fundamental/Machine Learning With Python for Beginner/Mini-Project.py
@@ -0,0 +1,6 @@
+# =====  Case Study: Promos for our e-commerce - Part - 1  =====
+# =====  Case Study: Promos for our e-commerce - Part - 2  =====
+# =====  Case Study: Promos for our e-commerce - Part - 3  =====
+# =====  Case Study: Promos for our e-commerce - Part - 4  =====
+# =====  Case Study: Promos for our e-commerce - Part - 5  =====
+# =====  Case Study: Promos for our e-commerce - Part - 6  =====
