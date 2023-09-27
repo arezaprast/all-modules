3b4021b2322b4637cc4ead7084bcce1e053253fd
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-09-27 13:41:06 +0700

    Add script

--- Python/Fundamental/Machine Learning With Python for Beginner/Chapter-4.py
+++ Python/Fundamental/Machine Learning With Python for Beginner/Chapter-4.py
@@ -1,4 +1,118 @@
 # =====  Pemodelan Permasalahan Klasifikasi dengan Logistic Regression  =====
+import pandas as pd
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/online_raw.csv')
+dataset.fillna(dataset.mean(), inplace = True)
+from sklearn.preprocessing import LabelEncoder
+LE = LabelEncoder()
+dataset['Month'] = LE.fit_transform(dataset['Month'])
+LE = LabelEncoder()
+dataset['VisitorType'] = LE.fit_transform(dataset['VisitorType'])
+X = dataset.drop(['Revenue'], axis = 1)
+y = dataset['Revenue']
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, classification_report
+#Call the classifier
+logreg = LogisticRegression()
+#Fit the classifier to the training data  
+logreg = logreg.fit(X_train, y_train)
+#Training Model: Predict 
+y_pred = logreg.predict(X_test)
+#Evaluate Model Performance
+print('Training Accuracy :', logreg.score(X_train, y_train))  
+print('Testing Accuracy :', logreg.score(X_test, y_test))  
+#confusion matrix
+print('\nConfusion matrix')  
+cm = confusion_matrix(y_test, y_pred)  
+print(cm)
+#classification report  
+print('\nClassification report')  
+cr = classification_report(y_test, y_pred) 
+print(cr)
+
 # =====  Tugas Praktek  =====
+import pandas as pd
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/online_raw.csv')
+dataset.fillna(dataset.mean(), inplace = True)
+from sklearn.preprocessing import LabelEncoder
+LE = LabelEncoder()
+dataset['Month'] = LE.fit_transform(dataset['Month'])
+LE = LabelEncoder()
+dataset['VisitorType'] = LE.fit_transform(dataset['VisitorType'])
+X = dataset.drop(['Revenue'], axis = 1)
+y = dataset['Revenue']
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+#splitting the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+#Call the classifier
+decision_tree = DecisionTreeClassifier()
+#Fit the classifier to the training data
+decision_tree = decision_tree.fit(X_train, y_train)
+#evaluating the decision_tree performance
+print('Training Accuracy :', decision_tree.score(X_train, y_train))
+print('Testing Accuracy :', decision_tree.score(X_test, y_test))
+
 # =====  Tugas Praktek  =====
+import pandas as pd
+housing = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/housing_boston.csv')
+#Data rescaling
+from sklearn import preprocessing
+data_scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
+housing[['RM','LSTAT','PTRATIO','MEDV']] = data_scaler.fit_transform(housing[['RM','LSTAT','PTRATIO','MEDV']])
+#getting dependent and independent variables
+X = housing.drop(['MEDV'], axis = 1)
+y = housing['MEDV']
+#checking the shapes
+print('Shape of X:', X.shape)
+print('Shape of y:', y.shape)
+#splitting the data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+#checking the shapes  
+print('Shape of X_train :', X_train.shape)
+print('Shape of y_train :', y_train.shape)
+print('Shape of X_test :', X_test.shape)
+print('Shape of y_test :', y_test.shape)
+#import regressor from Scikit-Learn
+from sklearn.linear_model import LinearRegression
+#Call the regressor
+reg = LinearRegression()
+#Fit the regressor to the training data  
+reg = reg.fit(X_train, y_train)
+#Apply the regressor/model to the test data  
+y_pred = reg.predict(X_test)
+
 # =====  Tugas Praktek  =====
+import pandas as pd
+housing = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/housing_boston.csv')
+from sklearn import preprocessing
+data_scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
+housing[['RM','LSTAT','PTRATIO','MEDV']] = data_scaler.fit_transform(housing[['RM','LSTAT','PTRATIO','MEDV']])
+X = housing.drop(['MEDV'], axis = 1)
+y = housing['MEDV']
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg = reg.fit(X_train,y_train)
+y_pred = reg.predict(X_test)
+from sklearn.metrics import mean_squared_error,  mean_absolute_error
+import numpy as np
+import matplotlib.pyplot as plt 
+#Calculating MSE, lower the value better it is. 0 means perfect prediction
+mse = mean_squared_error(y_test, y_pred)
+print('Mean squared error of testing set:', mse)
+#Calculating MAE
+mae = mean_absolute_error(y_test, y_pred)
+print('Mean absolute error of testing set:', mae)
+#Calculating RMSE
+rmse = np.sqrt(mse)
+print('Root Mean Squared Error of testing set:', rmse)
+#Plotting y_test dan y_pred
+plt.scatter(y_test, y_pred, c = 'green')
+plt.xlabel('Price Actual')
+plt.ylabel('Predicted value')
+plt.title('True value vs predicted value : Linear Regression')
+plt.show()
