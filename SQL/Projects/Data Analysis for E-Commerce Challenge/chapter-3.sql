58cd8c5c99b42340340ab6b481ae78bd562951f2
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-10-30 09:54:50 +0700

    Add script

--- SQL/Projects/Data Analysis for E-Commerce Challenge/chapter-3.sql
+++ SQL/Projects/Data Analysis for E-Commerce Challenge/chapter-3.sql
@@ -1,41 +1,41 @@
 -- 10 Transaksi terbesar user 12476
-select seller_id, buyer_id, total as nilai_transaksi, created_at as tanggal_transaksi
-from orders
-where buyer_id = 12476
-order by 3 desc
-limit 10;
+SELECT seller_id, buyer_id, total AS nilai_transaksi, created_at AS tanggal_transaksi
+FROM orders
+WHERE buyer_id = 12476
+ORDER BY 3 DESC
+LIMIT 10;
 
 -- Transaksi per bulan
-select EXTRACT(YEAR_MONTH FROM created_at) as tahun_bulan, count(1) as jumlah_transaksi, sum(total) as total_nilai_transaksi
-from orders
-where created_at >= '2020-01-01'
-group by 1
-order by 1;
+SELECT EXTRACT(YEAR_MONTH FROM created_at) AS tahun_bulan, COUNT(1) AS jumlah_transaksi, sum(total) AS total_nilai_transaksi
+FROM orders
+WHERE created_at >= '2020-01-01'
+GROUP BY 1
+ORDER BY 1;
 
 -- Pengguna dengan rata-rata transaksi terbesar di Januari 2020
-select buyer_id, count(1) as jumlah_transaksi, avg(total) as avg_nilai_transaksi
-from orders
-where created_at>='2020-01-01' and created_at<'2020-02-01'
-group by 1
-having count(1)>= 2 
-order by 3 desc
-limit 10;
+SELECT buyer_id, COUNT(1) AS jumlah_transaksi, avg(total) AS avg_nilai_transaksi
+FROM orders
+WHERE created_at>='2020-01-01' AND created_at<'2020-02-01'
+GROUP BY 1
+HAVING COUNT(1)>= 2 
+ORDER BY 3 DESC
+LIMIT 10;
 
 -- Transaksi besar di Desember 2019
-select nama_user as nama_pembeli, total as nilai_transaksi, created_at as tanggal_transaksi
-from orders
-inner join users on buyer_id = user_id
-where created_at>='2019-12-01' and created_at<'2020-01-01'
-and total >= 20000000
-order by 1;
+SELECT nama_user AS nama_pembeli, total AS nilai_transaksi, created_at AS tanggal_transaksi
+FROM orders
+INNER JOIN users ON buyer_id = user_id
+WHERE created_at>='2019-12-01' AND created_at<'2020-01-01'
+AND total >= 20000000
+ORDER BY 1;
 
 -- Kategori Produk Terlaris di 2020
-select category, sum(quantity) as total_quantity, sum(price) as total_price
-from orders
-inner join order_details using(order_id)
-inner join products using(product_id)
-where created_at>='2020-01-01'
-and delivery_at IS NOT NULL
-group by 1
-order by 2 desc
-limit 5;
+SELECT category, sum(quantity) AS total_quantity, sum(price) AS total_price
+FROM orders
+INNER JOIN order_details USING(order_id)
+INNER JOIN products USING(product_id)
+WHERE created_at>='2020-01-01'
+AND delivery_at IS NOT NULL
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5;
